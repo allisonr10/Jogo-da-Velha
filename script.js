6d874554b44f67442e9d67c6a58553c5243aae37
Allison Rodrigues <allison.ifg@gmail.com>
2020-11-12 08:57:19 -0300

    Lógica
    Fazendo a lógica da troca de jogadores.

---
+++ script.js
@@ -0,0 +1,27 @@
+let jogador = null
+
+let jogadorSelecionado = document.getElementById('jogador-selecionado')
+
+mudarJogador('X')
+
+function escolherQuadrado(id) {
+  var quadrado = document.getElementById(id)
+
+  quadrado.innerHTML = jogador
+  quadrado.style.color = '#000'
+
+  //checagem de jogador
+
+  if (jogador === 'X') {
+    jogador = 'O'
+  } else {
+    jogador = 'X'
+  }
+  mudarJogador(jogador)
+}
+
+function mudarJogador(valor) {
+  jogador = valor
+  jogadorSelecionado.innerHTML = jogador
+}
+7
